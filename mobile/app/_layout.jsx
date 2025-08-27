2e1e43ede7bb4c03eae1b14b27226b9eaf06246e
Dragos Rusu <86013739+Drragos@users.noreply.github.com>
2025-08-27 23:11:55 +0300

    add tabs and layouts

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,14 @@
 import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { SafeAreaView } from "react-native-safe-area-context";
+import SafeScreen from "../components/SafeScreen";
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeScreen>
         <Slot />
-      </SafeAreaView>
+      </SafeScreen>
     </ClerkProvider>
   );
 }
